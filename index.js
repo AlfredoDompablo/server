ad91f06c98c04aba4248cfc77709a4d7b6e3ecf0
AlfredoDompablo <oscardompablo@gmail.com>
2023-05-15 21:48:28 -0600

    Se corrigio problema de comunicacion con servidor, cambio de http a https

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const express = require('express');
 const mysql = require("mysql");
+const https = require('https');
+const fs = require('fs');
 const app = express();
-const cors =require("cors");
+const cors = require("cors");
 require("dotenv").config();
 
 const host_usr = process.env.HOST_DB;
 const password = process.env.PASS;
 const data_base = process.env.DB;
 
-// console.log(host_usr);
-// console.log(password);
-// console.log(data_base);
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/door-ipn.ddns.net/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/door-ipn.ddns.net/fullchain.pem')
+};
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: host_usr,
-    user:"root",
-    password: password,
-    database: data_base
+  host: host_usr,
+  user: "root",
+  password: password,
+  database: data_base
 });
 
-app.post("/create",(req,res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+app.post("/create", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    db.query('INSERT INTO usuario(email,password) VALUES(?,?)',[email,password],
-    (err,result) => {
-        if (err){
-            console.log(err);
-        }else{
-            res.send("Bienvenido :)")
-        }
+  db.query('INSERT INTO usuario(email,password) VALUES(?,?)', [email, password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Bienvenido :)")
+      }
     });
-})
+});
 
-
-app.listen(3001, ()=>{
-    console.log("servidor en el puerto 3001")
-})
+https.createServer(options, app).listen(3001, () => {
+  console.log("Servidor en el puerto 3001 con HTTPS habilitado");
+});
