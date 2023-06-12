ad726fbed697aa36683a4f65d6eb310658b5207a
AlfredoDompablo <oscardompablo@gmail.com>
2023-06-12 09:52:36 -0600

    Servidor funcionando

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ import {PORT} from './config.js'
 import cors  from "cors";
 import indexRoutes from "./routes/index.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import fs from "fs"
+import https from "https"
+
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());    
 
 // Configurar las opciones para el servidor HTTPS
-// const options = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/door-ipn.ddns.net/privkey.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/door-ipn.ddns.net/fullchain.pem')
-//   };
-  
-//   https.createServer(options, app).listen(3001, () => {
-//     console.log("Servidor en el puerto " + PORT + " con HTTPS habilitado");
-//   });
+const options = {
+    key: fs.readFileSync('./privkey.pem'),
+    cert: fs.readFileSync('./fullchain.pem')
+};
+
 app.use(indexRoutes);
 app.use(userRoutes);
-app.listen(PORT);
+  https.createServer(options, app).listen(3001, () => {
+    console.log("Servidor en el puerto " + PORT + " con HTTPS habilitado");
+  });
+//app.listen(PORT);
 console.log('Servidor en el puerto ' + PORT);
